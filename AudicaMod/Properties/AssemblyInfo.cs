083fb71c854e7688c089d653419cc32113f1a2be
Meeps <cummies@meeps.me>
2020-09-27 00:23:20 -0700

    Changed to a general song data loader

--- AudicaMod/Properties/AssemblyInfo.cs
+++ AudicaMod/Properties/AssemblyInfo.cs
@@ -4,23 +4,23 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using AudicaModding;
 
-[assembly: AssemblyTitle(CustomSongDataLoader.BuildInfo.Name)]
+[assembly: AssemblyTitle(SongDataLoader.BuildInfo.Name)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(CustomSongDataLoader.BuildInfo.Company)]
-[assembly: AssemblyProduct(CustomSongDataLoader.BuildInfo.Name)]
-[assembly: AssemblyCopyright("Created by " + CustomSongDataLoader.BuildInfo.Author)]
-[assembly: AssemblyTrademark(CustomSongDataLoader.BuildInfo.Company)]
+[assembly: AssemblyCompany(SongDataLoader.BuildInfo.Company)]
+[assembly: AssemblyProduct(SongDataLoader.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + SongDataLoader.BuildInfo.Author)]
+[assembly: AssemblyTrademark(SongDataLoader.BuildInfo.Company)]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 //[assembly: Guid("")]
-[assembly: AssemblyVersion(CustomSongDataLoader.BuildInfo.Version)]
-[assembly: AssemblyFileVersion(CustomSongDataLoader.BuildInfo.Version)]
+[assembly: AssemblyVersion(SongDataLoader.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(SongDataLoader.BuildInfo.Version)]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: MelonInfo(typeof(CustomSongDataLoader), CustomSongDataLoader.BuildInfo.Name, CustomSongDataLoader.BuildInfo.Version, CustomSongDataLoader.BuildInfo.Author, CustomSongDataLoader.BuildInfo.DownloadLink)]
+[assembly: MelonInfo(typeof(SongDataLoader), SongDataLoader.BuildInfo.Name, SongDataLoader.BuildInfo.Version, SongDataLoader.BuildInfo.Author, SongDataLoader.BuildInfo.DownloadLink)]
 
 
 // Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
 // If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
 // Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
-[assembly: MelonGame(null, null)]
+[assembly: MelonGame("Harmonix Music Systems, Inc.", "Audica")]
--- AudicaMod/src/SongDataLoader.cs
+++ AudicaMod/src/SongDataLoader.cs
@@ -10,42 +10,42 @@ using System.IO.Compression;
 
 namespace AudicaModding
 {
-    public class CustomSongDataLoader : MelonMod
+    public class SongDataLoader : MelonMod
     {
 
         private static bool SongDataLoaded = false;
 
         //dictionary with song id to custom data
-        private static Dictionary<string, CustomData> AllCustomData = new Dictionary<string, CustomData>();
+        public static Dictionary<string, SongData> AllSongData = new Dictionary<string, SongData>();
 
         //load custom data when song list is enabled
         [HarmonyPatch(typeof(SongSelect), "OnEnable", new Type[0])]
-        private static class LoadDiffNamesOnSongSelectLoad
+        private static class LoadDataOnSongSelectLoad
         {
             private static void Postfix(DifficultySelect __instance)
             {
-                LoadCustomData();
+                LoadSongData();
             }
 
         }
 
         public static class BuildInfo
         {
-            public const string Name = "CustomSongDataLoader";  // Name of the Mod.  (MUST BE SET)
+            public const string Name = "SongDataLoader";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "MeepsKitten"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
             public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
 
-        private class NRBookmark
+        public class NRBookmark
         {
             int type;
             float xPosMini;
             float xPosTop;
         }
 
-        private class CustomData
+        public class SongData
         {
             //default game data (so it wont end up in our extra data dictionary)
             public string songID { get; set; }
@@ -67,18 +67,17 @@ namespace AudicaModding
             public string songEndEvent { get; set; }
             public float prerollSeconds { get; set; }
             public bool useMidiForCues { get; set; }
-
             public float songEndPitchAdjust { get; set; }
-
             public string highScoreEvent { get; set; }
             public bool hidden { get; set; }
             public string author { get; set; }
             public int offset { get; set; }
             public float previewStartSeconds { get; set; }
-            //public NRBookmark[] bookmarks { get; set; }
+
+           public NRBookmark[] bookmarks { get; set; }
 
             [JsonExtensionData]
-            public IDictionary<string, JToken> _extraJsonData { get; set; }
+            private IDictionary<string, JToken> _extraJsonData { get; set; }
 
             public bool HasCustomData()
             {
@@ -88,14 +87,27 @@ namespace AudicaModding
                     return false;
             }
 
+
+            //Tell you if a specified song has data for a specified key
+            public bool SongHasCustomDataKey(string key)
+            {
+                    return _extraJsonData.ContainsKey(key);
+            }
+
+            //generic function that gets data for a specific song that corresponds to a given key
+            public T GetCustomData<T>(string key)
+            {
+                return _extraJsonData[key].ToObject<T>();
+            }
+
         }
 
         //When called, this function will update 'AllCustomData' with data from every song installed (already called on [HarmonyPatch(typeof(SongSelect), "OnEnable", new Type[0])])
-        public static void LoadCustomData()
+        private static void LoadSongData()
         {
             if (!SongDataLoaded)
             {
-                AllCustomData = new Dictionary<string, CustomData>();
+                AllSongData = new Dictionary<string, SongData>();
 
                 foreach (SongList.SongData data in SongList.I.songs.ToArray())
                 {
@@ -108,15 +120,11 @@ namespace AudicaModding
                             Stream songData = entry.Open();
                             StreamReader reader = new StreamReader(songData);
                             string descDump = reader.ReadToEnd();
-                            CustomData JSONData = new CustomData();
-                            JSONData = JsonConvert.DeserializeObject<CustomData>(descDump);
+                            SongData JSONData = new SongData();
+                            JSONData = JsonConvert.DeserializeObject<SongData>(descDump);
                             //MelonLogger.Log(descDump);
 
-                            if (JSONData.HasCustomData())
-                            {
-                                AllCustomData[data.songID] = JSONData;
-                                
-                            }
+                            AllSongData[data.songID] = JSONData;
 
                         }
                     }
@@ -126,27 +134,10 @@ namespace AudicaModding
             }
         }
 
-        //Tells you if there is any custom song data present for a specified song
-        public static bool SongHasCustomData(string songId)
+        public static void ReloadSongData()
         {
-            return AllCustomData.ContainsKey(songId);
-        }
-
-        //Tell you if a specified song has data for a specified key
-        public static bool SongHasCustomDataKey(string songId, string key)
-        {
-            if (SongHasCustomData(songId))
-            {
-                return AllCustomData[songId]._extraJsonData.ContainsKey(key);
-            }
-            else
-                return false;
-        }
-
-        //generic function that gets data for a specific song that corresponds to a given key
-        public static T GetCustomData<T>(string songID, string key)
-        {
-            return AllCustomData[songID]._extraJsonData[key].ToObject<T>();
+            SongDataLoaded = false;
+            LoadSongData();
         }
     }
 }
